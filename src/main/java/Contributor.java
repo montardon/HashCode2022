84ea31da3dbab415f52a10cf75efa7fcb5af52c8
Julien <julien.lacoste@geotoolkit.net>
2022-02-24 21:38:35 +0100

    Hash

--- src/main/java/Contributor.java
+++ src/main/java/Contributor.java
@@ -1,7 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Contributor {
     public String name;
     List<Skill> skills = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contributor that = (Contributor) o;
+
+        if (!name.equals(that.name)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
