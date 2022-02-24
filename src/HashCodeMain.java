e1ae80504f58c08aedcb0e55579938f88ffed82a
Julien <julien.lacoste@geotoolkit.net>
2022-02-24 19:11:46 +0100

    2ndcommit

---
+++ src/HashCodeMain.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class HashCodeMain {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scan = new Scanner(new File("/home/jlacoste/CODE/HashCode/a_an_example.in"))) {
+            while (scan.hasNextLine()) {
+
+            }
+        }
+    }    
+
+}
