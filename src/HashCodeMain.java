a80fe0ab73fd1c4807ac4cbe48bdcfacb52f2a33
Julien <julien.lacoste@geotoolkit.net>
2022-02-24 19:45:23 +0100

    4rdCommit

--- src/HashCodeMain.java
+++ src/HashCodeMain.java
@@ -11,9 +11,10 @@ public class HashCodeMain {
     
     public static void main(String[] args) throws FileNotFoundException {
         Map<String, Contributor> contributors = new HashMap<>();
-        int nbContrib = 0;
+        List<Project> projects = new ArrayList<>();
         int nbProject = 0;
-        try (Scanner scan = new Scanner(new File("/home/jlacoste/CODE/HashCode/a_an_example.in"))) {
+        int nbContrib = 0;
+        try (Scanner scan = new Scanner(new File("/home/jlacoste/CODE/HashCode/a_an_example.in.txt"))) {
             while (scan.hasNextLine()) {
                 int[] nbs = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
                 nbContrib = nbs[0];
@@ -23,10 +24,38 @@ public class HashCodeMain {
                     Contributor ctrb = new Contributor();
                     String[] line = scan.nextLine().split(" ");
                     String ctrName = line[0];
+                    ctrb.name = ctrName;
+                    int nbSkills = Integer.parseInt(line[1]);
+                    for (int s = 0 ; s < nbSkills ; ++s) {
+                        String[] lineSk = scan.nextLine().split(" ");
+                        Skill skill = new Skill();
+                        skill.name = lineSk[0];
+                        skill.level = Integer.parseInt(lineSk[1]);
+                        ctrb.skills.add(skill);
+                    }
+                    contributors.put(ctrName, ctrb);
+                }
 
+                // projects
+                for (int idx = 0 ; idx < nbProject ; ++idx) {
+                    Project proj = new Project();
+                    String[] lineProj = scan.nextLine().split(" ");
+                    proj.name = lineProj[0];
+                    proj.duration = Integer.parseInt(lineProj[1]);
+                    proj.score = Integer.parseInt(lineProj[2]);
+                    proj.bestBefore = Integer.parseInt(lineProj[3]);
+                    int nbRoles = Integer.parseInt(lineProj[4]);
+                    for (int r = 0 ; r < nbRoles ; ++r) {
+                        String[] line = scan.nextLine().split(" ");
+                        Project.Role role = new Project.Role();
+                        role.name = line[0];
+                        role.minLevel = Integer.parseInt(line[1]);
+                        proj.roles.add(role);
+                    }
+                    projects.add(proj);
                 }
             }
         }
-    }    
+    }   
 
 }
